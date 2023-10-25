3729668e2718b611ec97176c9c677662e7e2d00e
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-10-25 21:19:28 +0330

    feat : add createdby to baseModel

--- Src/Product.Domain/Entities/BaseModel.cs
+++ Src/Product.Domain/Entities/BaseModel.cs
@@ -7,29 +7,35 @@ public abstract class BaseModel<T>
 {
     public T Id { get;  set; }
     public long CreateDate { get; private set; }
+    public long? CreatedBy { get; private set; }
     public long? UpdateDate { get; private set; }
+    public long? UpdatedBy { get; private set; }
     public long? DeleteDate { get; private set; }
+    public long? DeletedBy { get; private set; }
     public string? CreateIp { get; private set; }
     public string? UpdateIp { get; private set; }
     public string? DeleteIp { get; private set; }
 
-    public virtual void Create(string? ip = null)
+    public virtual void Create(string? ip = null , long? createdBy = null)
     {
         this.CreateDate = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        this.CreatedBy = createdBy;
         ValidateIp(ip);
         this.CreateIp = ip;
     }
     
-    public virtual void Update(string? ip = null)
+    public virtual void Update(string? ip = null, long? updatedBy = null)
     {
         this.UpdateDate = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        this.UpdatedBy = updatedBy;
         ValidateIp(ip);
         this.UpdateIp = ip;
     }
     
-    public virtual void Delete(string? ip = null)
+    public virtual void Delete(string? ip = null, long? deletedBy = null)
     {
         this.DeleteDate = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        this.DeletedBy = deletedBy;
         ValidateIp(ip);
         this.DeleteIp = ip;
     }
